703f3da63299c338e21289755586a1e78f206c29
jujube6363 <113395821+jujube6363@users.noreply.github.com>
2024-03-23 16:24:38 -0500

    Update CarScript.cs

--- CarScript.cs
+++ CarScript.cs
@@ -9,9 +9,10 @@ public class CarScript : MonoBehaviour
     public Rigidbody2D myRigidBody;
     public float angle;
     public float motorIn;
-    public RoadScript roadscript;   
-
-    SerialPort data_stream = new SerialPort("COM5", 9600);
+    public RoadScript roadscript;
+    public LogicScript logic;   
+    public BoxCollider boxcollider;
+    public SerialPort data_stream = new SerialPort("COM5", 9600);
     public string receivedstring;
 
     // Start is called before the first frame update
@@ -19,9 +20,26 @@ public class CarScript : MonoBehaviour
     {
         //transform.Rotate(new Vector3(0, 0, 90));
         myRigidBody = GetComponent<Rigidbody2D>();
-
+        boxcollider = GetComponent<BoxCollider>();
+        logic = GameObject.FindGameObjectWithTag("Logic").GetComponent<LogicScript>();
         data_stream.Open();
+        
     }
+
+    void OnTriggerEnter(Collider oname){
+        Debug.Log("entered collision with: something");
+        myRigidBody.isKinematic = false;
+        boxcollider.isTrigger = false;
+    }
+
+    void OnTriggerEnterStay(Collider oname){
+        Debug.Log("entered collision with: something");
+    }
+    void OnTriggerEnterExit(Collider oname){
+        Debug.Log("entered collision with: something");
+
+    }
+
 
     // Update is called once per frame
     void Update()
@@ -34,6 +52,7 @@ public class CarScript : MonoBehaviour
             motorIn = (float)(int.Parse(recData[0]) * 1.4);
             motorIn *= (float)((150.0 - roadscript.roadXCoord + 150) / 150);
             float dialIn = int.Parse(recData[1]);
+            Debug.Log(dialIn);
             angle = myRigidBody.transform.rotation.eulerAngles.z;
             
             double xVel = Math.Cos(angle*Math.PI/180);
@@ -41,13 +60,15 @@ public class CarScript : MonoBehaviour
             Vector3 velocitys = new Vector3((float)xVel * 0, (float)yVel, 0);
 
             velocitys *= motorIn;
+            
             myRigidBody.velocity = velocitys;
+
 
             dialIn /= 67;
             dialIn -= 5;
             dialIn *= 18;
             //transform.Rotate(new Vector3(0, 0, dialIn));
-            transform.rotation = Quaternion.Euler(0, 0, (float)dialIn);
+            if (!(dialIn + 90 < 2)) transform.rotation = Quaternion.Euler(0, 0, (float)dialIn);
         } catch (Exception e) {
             Debug.Log(e);
         }
@@ -55,4 +76,12 @@ public class CarScript : MonoBehaviour
     
 
     }
+    // private void OnCollisionEnter2D(Collision2D collision){
+    //     if (collision.gameObject.CompareTag("Obstacle")) {
+    //         carIsAlive = false;
+    //         Debug.Log("triggered by: " + collision);
+    //         logic.gameOver();
+    //     }
+            
+    // }
 }
